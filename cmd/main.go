ee3f5b1a99a8ee20a44da7c290f7a8202b3ceae5
BedivereZero <BedivereZero@gmail.com>
2021-10-28 21:11:23 +0800

    Add /starttime

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/BedivereZero/test-cicd/version"
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,11 @@ func main() {
 	})
 	r.GET("/version", func(c *gin.Context) {
 		c.JSON(http.StatusOK, version.Get())
+	})
+
+	starttime := time.Now()
+	r.GET("/starttime", func(c *gin.Context) {
+		c.String(http.StatusOK, starttime.Format(time.RFC3339))
 	})
 
 	r.Run(":1080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
